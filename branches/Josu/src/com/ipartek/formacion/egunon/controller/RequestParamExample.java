9f9f9e21bd5bcb2265cfdfbc26ab737cef75f2e9
josumeri@gmail.com <josumeri@gmail.com@2f63a060-3793-c055-0ba4-19c003e06cb0>
2014-03-12 11:19:59 +0000

    Creación de mi branch en esta rama

---
+++ branches/Josu/src/com/ipartek/formacion/egunon/controller/RequestParamExample.java
@@ -0,0 +1,72 @@
+package com.ipartek.formacion.egunon.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RequestParamExample
+ */
+public class RequestParamExample extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RequestParamExample() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String firstName = request.getParameter("firstname");
+		String lastName = request.getParameter("lastname");
+		
+		response.setContentType("text/html");
+	        PrintWriter out = response.getWriter();
+	        out.println("<html>");
+	        out.println("<head>");
+	        out.println("<title>Request Parameters Example</title>");
+	        out.println("</head>");
+	        out.println("<body>");
+	        out.println("<h3>Request Parameters Example</h3>");
+	        out.println("Parameters in this request:<br>");
+	        if (firstName != null || lastName != null) {
+	            out.println("First Name:");
+	            out.println(" = " + firstName + "<br>");
+	            out.println("Last Name:");
+	            out.println(" = " + lastName);
+	        } else {
+	            out.println("No Parameters, Please enter some");
+	        }
+	        out.println("<P>");
+	        out.print("<form action=\"");
+	        out.print("requestraram\" ");
+	        out.println("method=POST>");
+	        out.println("First Name:");
+	        out.println("<input type=text size=20 name=firstname>");
+	        out.println("<br>");
+	        out.println("Last Name:");
+	        out.println("<input type=text size=20 name=lastname>");
+	        out.println("<br>");
+	        out.println("<input type=submit>");
+	        out.println("</form>");
+	        out.println("</body>");
+	        out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
